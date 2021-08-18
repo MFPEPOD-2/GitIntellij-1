0675b48ba0b5ea2a91fd660e9e7837d6d47f785f
chaithu330 <venkymustang330@gmail.com>
2021-08-18 12:41:05 +0530

    Added some util methods
    Added hi() and an util class

--- src/com/demo/git/Main.java
+++ src/com/demo/git/Main.java
@@ -1,9 +1,17 @@
 package com.demo.git;
 
+import com.demo.git.util.GitUtil;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        GitUtil util = new GitUtil();
+        util.utilMethod();
         System.out.println("hello this is a demo project to learn git.");
+    }
+
+    public void hi(){
+        System.out.println("Hi Git this is a new user from Intellij");
     }
 }
---
+++ src/com/demo/git/util/GitUtil.java
@@ -0,0 +1,8 @@
+package com.demo.git.util;
+
+public class GitUtil {
+
+    public void utilMethod(){
+        System.out.println("This is just a sample util method....");
+    }
+}
